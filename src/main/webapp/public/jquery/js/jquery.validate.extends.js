274f63490727eb63d6165f28d44a33748570c3a0
Bulain <bulain@gmail.com>
2011-09-17 12:49:16 +0800

    remove public folder

--- src/main/webapp/public/jquery/js/jquery.validate.extends.js
+++
@@ -1,4 +0,0 @@
-jQuery.validator.addMethod("time", function(value, element, param) {    
-    return this.optional(element) || /^([0-1]\d|2[0-3]):[0-5]\d$/.test(value);    
-}, 'Please enter a valid time.');
-jQuery.validator.addClassRules("time", {time: true});
